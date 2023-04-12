2b81ed49c8dd118c588aa032a0830c07784a0d83
David Williams <davidlwilliams423@gmail.com>
2023-04-12 15:59:41 -0400

    Add files via upload

---
+++ AccessibilityDemo/AccessibilityDemo/AccessibilityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityDemoApp.swift
+//  AccessibilityDemo
+//
+//  Created by David Williams on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
