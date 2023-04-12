0672cb83c5c64ebbe3b3fbd73d9cc1b093632e61
David Williams <davidlwilliams423@gmail.com>
2023-04-12 17:34:15 -0400

    final x 4

--- AccessibilityDemo/AccessibilityDemo/ContentView.swift
+++ AccessibilityDemo/AccessibilityDemo/ContentView.swift
@@ -9,6 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State var fontSize = 30.0
+    var sizeDescription: String {
+            if fontSize < 25 {
+                return "Too small"
+            } else if fontSize > 35 {
+                return "Too big"
+            } else {
+                return "OK"
+            }
+        }
     var body: some View {
         VStack {
             Title()
@@ -16,12 +25,23 @@ struct ContentView: View {
             Spacer()
 
             CellView(fontSize: $fontSize)
+            CellView(fontSize: $fontSize)
+            CellView(fontSize: $fontSize)
             
             Spacer()
 
             VStack(alignment: .leading) {
-                Text("Font Size")
-                    .bold()
+                HStack {
+                    Text("Font Size")
+                        .bold()
+                    Spacer()
+                    HStack {
+                        Image(systemName: sizeDescription
+                              != "OK" ? "exclamationmark.triangle.fill" : "checkmark.circle.fill")
+                            .foregroundColor(sizeDescription != "OK" ? .yellow : .green)
+                        Text(sizeDescription)
+                    }
+                }
                 Slider(value: $fontSize, in: 20 ... 40)
             }
             .padding()
